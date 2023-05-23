d4f1a80f460d3f00bd99f965b3decfe9e800edf8
Tejas Rote <tejas292002@gmail.com>
2023-05-23 22:52:32 +0530

    desktop--done

--- FAQ_Accordian/faq_app/src/App.js
+++ FAQ_Accordian/faq_app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MainCard from './components/MainCard';
+// import Faq from './components/Faq';
+const App = () => {
+    return (
+        <div className='gradient__bg' style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+        }}>
+
+            <MainCard />
+            {/* <Faq /> */}
+
+        </div>
+
+    )
 }
 
-export default App;
+export default App
