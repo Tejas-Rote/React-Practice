273d351e0415d78343c51502b97069db21e054a2
Tejas Rote <tejas292002@gmail.com>
2023-05-24 10:24:52 +0530

    done

--- FAQ_Accordian/faq_app/src/App.js
+++ FAQ_Accordian/faq_app/src/App.js
@@ -5,12 +5,7 @@ import MainCard from './components/MainCard';
 // import Faq from './components/Faq';
 const App = () => {
     return (
-        <div className='gradient__bg' style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-        }}>
+        <div className='gradient__bg' >
 
             <MainCard />
             {/* <Faq /> */}
